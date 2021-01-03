8a171cc4acf33600647cf97132ae1c3b0a5d2b46
Geniecko <radzioo159@gmail.com>
2021-01-03 18:57:08 +0100

    Updates

---
+++ precache-manifest.33f74626df35a82381bfcb3f56f79dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce4313171ceda9556d59",
+    "url": "/react_taskmanager/static/css/main.df71ecde.chunk.css"
+  },
+  {
+    "revision": "ce4313171ceda9556d59",
+    "url": "/react_taskmanager/static/js/main.b43c7a48.chunk.js"
+  },
+  {
+    "revision": "ec8c619a20ab18a3303a",
+    "url": "/react_taskmanager/static/js/runtime~main.c6963d46.js"
+  },
+  {
+    "revision": "27df1ec9cb4828c368b4",
+    "url": "/react_taskmanager/static/js/2.6bdc0443.chunk.js"
+  },
+  {
+    "revision": "284a3cad9f03a2b61e35ae44d3190dca",
+    "url": "/react_taskmanager/index.html"
+  }
+];
